f84d557085d1bd81f6fde0d846fe3075aeda1c8f
Thiyagarajan <thiyagarajan@Thiyagarajans-MacBook-Pro.local>
2022-08-10 01:10:45 +0530

    first commit

---
+++ form/three.js
@@ -0,0 +1,15 @@
+let finame = document.querySelector(".firstname");
+let laname = document.querySelector(".lastname");
+let rlnumber= document.querySelector(".rlnumber");
+let pob= document.querySelector(".pob");
+
+
+function send(){
+console.log(finame.value);
+console.log(laname.value);
+	console.log(rlnumber.value);
+	console.log(pob.value);
+}
+
+
+
